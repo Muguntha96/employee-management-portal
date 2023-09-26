1f9321dd1401859c1efef299f07001ad15da0048
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-09-25 21:51:23 -0300

    set all the input fields are required in add employee page

--- models/employee.js
+++ models/employee.js
@@ -3,19 +3,21 @@ import mongoose from "mongoose"
 const Schema=mongoose.Schema
 
 const employeeSchema=new Schema({
-  firstName:String,
-  lastName:String,
-  dob:Date,
-  address:String,
-  salary:Number,
-  phoneNumber:String,
+  
+  firstName:{type:String,required:true},
+  lastName:{type:String,required:true},
+  dob:{type:Date,required:true},
+  address:{type:String,required:true},
+  salary:{type:Number,required:true},
+  phoneNumber:{type:Number,required:true},
   department:{
     type:String,
+    required:true,
     enum:['Admin','Finance','Support','HR','IT']
   },
   manager:{ type: Schema.Types.ObjectId, ref: 'Profile' },
-  reviews:[{type: Schema.Types.ObjectId, ref: 'Review'}]
- },{
+  reviews:[{type: Schema.Types.ObjectId, ref: 'Review'}],
+},{
   timestamps:true
 })
 
